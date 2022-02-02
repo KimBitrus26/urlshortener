7f51c98ca0ab646c386bd4e0ea3ec15f33234948
KimBitrus26 <bitruschoji4real@gmail.com>
2022-02-02 07:49:25 +0100

    built a url shortener app

--- shortenurltask/settings.py
+++ shortenurltask/settings.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -11,7 +12,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-y)2#+-j65^6od@g@9*h56-jwt9v7y4$38fl@iro_ty3*&b005y'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
